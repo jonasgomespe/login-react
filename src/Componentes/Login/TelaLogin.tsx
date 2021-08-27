601e2b3b28927474e907c3469eaeae072f7f7a46
upjdesign <38765631+upjdesign@users.noreply.github.com>
2021-08-27 11:27:53 -0300

    Foi adcionado um novo design a área de login

---
+++ src/Componentes/Login/TelaLogin.tsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import './TelaLogin.css';
+
+export default function TelaLogin(){
+
+    const [nomeUsuario, setNomeUsuario]    = useState('');
+    const [senhaUsuario, setSenhaUsuario]  = useState('');
+    
+    function digitacaoInput(e:any,tipo?:string){
+
+        if(tipo == 'login')
+            setNomeUsuario(e.target.value);
+
+        if(tipo == 'senha')
+            setSenhaUsuario(e.target.value);
+
+    }
+
+    function defaultForm(event:any){
+        event.preventDefault();
+    }
+
+    return (
+        <div className="container-login">
+            <div className="alinhamento">
+                <div>
+                    <form name="form-login" onSubmit={(event)=>defaultForm(event)}>
+                        <div>
+                                <h1>
+                                    Área de usuários
+                                </h1>
+                                <h4>
+                                    Sistema de gerencimento de funcionarios da empresa em geral
+                                </h4>
+                        </div>
+                        <div>
+                            <input type="text" name="sd" value={nomeUsuario} onChange={(e)=>digitacaoInput(e,'login')} placeholder=" Insira seu nome " id="sd" />
+                        </div>
+                        <div>
+                            <input type="password" name="fe" value={senhaUsuario} onChange={(e)=>digitacaoInput(e,'senha')} placeholder=" Insira sua senha " id="fe" />
+                        </div>
+                        <div>
+                            <input type="submit" value=" Entrar " id="yui" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
